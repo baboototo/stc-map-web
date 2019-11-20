962bbedc06e7e1fc91935506d82bca45139410c1
baboototo <baboototo@gmail.com>
2019-11-20 16:55:10 +0900

    환경 설정파일 변경

--- src/main/java/com/stc/map/MapApplication.java
+++ src/main/java/com/stc/map/MapApplication.java
@@ -3,13 +3,19 @@ package com.stc.map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ServletComponentScan
-@SpringBootApplication(scanBasePackages = "com.stc.map")
-public class MapApplication {
 
+@SpringBootApplication
+public class MapApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MapApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(MapApplication.class, args);
     }
